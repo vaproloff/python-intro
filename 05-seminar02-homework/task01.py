f220b3c534ce69a49f98576ff5ee17a1d511b8c9
Konstantin Pivnov <vaproloff@ya.ru>
2022-11-25 12:16:05 +0300

    added seminar 2 homework task 1 solution

---
+++ 05-seminar02-homework/task01.py
@@ -0,0 +1,9 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр. (Сделать для строки)
+
+num_input = input('Введите число: ')
+num = num_input.replace('.', '').replace(',', '')
+sum_digits = 0
+for i in range(len(num)):
+    sum_digits += int(num[i])
+print(f'Сумма цифр числа {num_input}: {sum_digits}')
